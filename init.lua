7cf751087d729424e84dc83f6d2ebc5728b69852
Parsa <parsam280xyz@gmail.com>
2025-12-04 10:20:52 +0330

    fix:removed furusato configuration

--- init.lua
+++ init.lua
@@ -2,14 +2,4 @@ local base = require 'base'
 local bufferline = require 'custom.plugins.init'
 local keymaps = require 'keymaps'
 
--- Furusato theme configuration
-vim.g.setup = {
-  transparent = true, -- Enable transparency
-  italic_comments = true, -- Italicize comments (default: true)
-  italic_keywords = false, -- Italicize keywords (if, for, while, etc.)
-  italic_functions = false, -- Italicize function names
-  italic_variables = false, -- Italicize variables
-}
 
--- Load colorscheme
-vim.cmd.colorscheme 'furusato'
